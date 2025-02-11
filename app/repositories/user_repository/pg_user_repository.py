995ddd032463ef5681d0abb8062a4e47b09f6c44
allenyuchen <allenyuchen@hotmail.com>
2025-02-11 08:39:07 -0800

    chore: User AsyncSession for better type hinting

--- app/repositories/user_repository/pg_user_repository.py
+++ app/repositories/user_repository/pg_user_repository.py
@@ -4,21 +4,26 @@ from typing import Optional
 
 from loguru import logger
 from sqlalchemy import insert, select, update
-from sqlalchemy.ext.asyncio import AsyncConnection, AsyncEngine
+from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker
 
 from app.models.user import Session, SessionDb, User, UserCreate, UserDb
 
 
 class PgUserRepo:
     def __init__(self, engine: AsyncEngine) -> None:
-        self.engine = engine
-        self.existing_transaction: Optional[AsyncConnection] = None
+        self.async_session_maker = async_sessionmaker(engine)
+        self.existing_transaction: Optional[AsyncSession] = None
 
     @asynccontextmanager
     async def transaction(self):
-        async with self.engine.begin() as conn:
-            self.existing_transaction = conn
-            yield
+        try:
+            assert self.existing_transaction is None
+            logger.info("Creating new transaction")
+            async with self.async_session_maker() as conn:
+                async with conn.begin():
+                    self.existing_transaction = conn
+                    yield
+        finally:
             self.existing_transaction = None
 
     @asynccontextmanager
@@ -27,27 +32,39 @@ class PgUserRepo:
             logger.info("Using existing transaction")
             yield self.existing_transaction
         else:
-            logger.info("Creating new transaction")
-            async with self.engine.begin() as conn:
-                yield conn
+            async with self.transaction():
+                assert self.existing_transaction is not None
+                yield self.existing_transaction
 
     async def get_user_by_id(self, user_id: int) -> Optional[User]:
         async with self.get_transaction() as conn:
-            stmt = select(UserDb).where(UserDb.id == user_id)
+            stmt = (
+                select(UserDb)
+                .where(UserDb.id == user_id, UserDb.deleted_at == None)  # noqa
+                .limit(1)
+            )
             result = await conn.execute(stmt)
-            user = result.fetchone()
+            user = result.scalar()
             if user is not None:
-                return User.model_validate(user)
-            return None
+                user = User.model_validate(user)
+            return user
 
     async def get_user_by_email(self, email: str) -> Optional[User]:
         async with self.get_transaction() as conn:
-            stmt = select(UserDb).where(UserDb.email == email)
+            stmt = (
+                select(UserDb)
+                .where(
+                    UserDb.email == email,
+                    UserDb.deleted_at == None,  # noqa
+                )
+                .limit(1)
+            )
             result = await conn.execute(stmt)
-            user = result.first()
+            user = result.scalar()
+            # user = await result.scalar()
             if user is not None:
-                return User.model_validate(user)
-            return None
+                user = User.model_validate(user)
+            return user
 
     async def create_user(self, user: UserCreate) -> User:
         async with self.get_transaction() as conn:
@@ -81,15 +98,20 @@ class PgUserRepo:
 
     async def get_session(self, user_id: int, session_id: str) -> Optional[Session]:
         async with self.get_transaction() as conn:
-            stmt = select(SessionDb).where(
-                SessionDb.user_id == user_id,
-                SessionDb.token == session_id,
+            stmt = (
+                select(SessionDb)
+                .where(
+                    SessionDb.user_id == user_id,
+                    SessionDb.token == session_id,
+                    SessionDb.deleted_at == None,  # noqa
+                )
+                .limit(1)
             )
             result = await conn.execute(stmt)
-            session = result.first()
+            session = result.scalar()
             if session is not None:
-                return Session.model_validate(session)
-            return None
+                session = Session.model_validate(session)
+            return session
 
     async def commit(self):
         async with self.get_transaction() as conn:
